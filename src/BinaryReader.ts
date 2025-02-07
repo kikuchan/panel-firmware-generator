516910091b3ae792887ce8ac1c552b2137f60532
Hironori KIKUCHI <kikuchan98@gmail.com>
2025-02-07 13:04:51 +0900

    update: BinaryReader

--- src/BinaryReader.ts
+++ src/BinaryReader.ts
@@ -1,57 +1,26 @@
 export class BinaryReader {
-  buffer: DataView<ArrayBufferLike>;
+  view: DataView;
   position: number;
 
   constructor(u8: ArrayBuffer | Uint8Array | Uint8ClampedArray) {
     if (u8 instanceof ArrayBuffer) {
-      this.buffer = new DataView(u8);
+      this.view = new DataView(u8);
     } else {
-      this.buffer = new DataView(u8.buffer, u8.byteOffset, u8.byteLength);
+      this.view = new DataView(u8.buffer, u8.byteOffset, u8.byteLength);
     }
     this.position = 0;
   }
 
-  readByte() {
-    return this.buffer.getUint8(this.position++);
+  get size() {
+    return this.view.byteLength;
   }
 
-  readBe16() {
-    const r = this.buffer.getUint16(this.position, false);
-    this.position += 2;
-    return r;
-  }
-
-  readBe32() {
-    const r = this.buffer.getUint32(this.position, false);
-    this.position += 4;
-    return r;
-  }
-
-  readBe64() {
-    const h = this.readBe32();
-    const l = this.readBe32();
-    return (BigInt(h) << 32n) | (BigInt(l) << 0n);
-  }
-
-  readBytes(n?: number) {
-    const pos = this.buffer.byteOffset + this.position;
-    if (n === undefined) n = this.buffer.byteLength - this.position;
-    this.position += n;
-    return this.buffer.buffer.slice(pos, pos + n);
-  }
-
-  readString() {
-    const size = this.buffer.byteLength - this.position;
-    for (let i = 0; i < size; i++) {
-      if (this.buffer.getUint8(this.position + i) === 0) {
-        return new TextDecoder().decode(this.readBytes(i + 1).slice(0, -1));
-      }
-    }
-    return undefined;
+  get remain() {
+    return this.size - this.position;
   }
 
   eof() {
-    return this.position >= this.buffer.byteLength;
+    return this.remain <= 0;
   }
 
   seek(n: number) {
@@ -69,5 +38,75 @@ export class BinaryReader {
       return this.skip(n - (this.position % n));
     }
     return this;
+  }
+
+  peekBuffer(n?: number) {
+    if (n === undefined) n = this.remain;
+    if (this.remain < n) return undefined; // EOF
+
+    const pos = this.view.byteOffset + this.position;
+    return this.view.buffer.slice(pos, pos + n);
+  }
+
+  readByte() {
+    return this.view.getUint8(this.position++);
+  }
+
+  readBe16() {
+    const r = this.view.getUint16(this.position, false);
+    this.position += 2;
+    return r;
+  }
+
+  readBe32() {
+    const r = this.view.getUint32(this.position, false);
+    this.position += 4;
+    return r;
+  }
+
+  readBe64() {
+    const h = this.readBe32();
+    const l = this.readBe32();
+    return (BigInt(h) << 32n) | (BigInt(l) << 0n);
+  }
+
+  readBytes(n?: number) {
+    const buffer = this.peekBuffer(n);
+    if (buffer) this.skip(buffer.byteLength);
+    return buffer;
+  }
+
+  #strlen() {
+    const remain = this.remain;
+    for (let i = 0; i < remain; i++) {
+      if (this.view.getUint8(this.position + i) === 0) {
+        return i;
+      }
+    }
+    return undefined; // No termination
+  }
+
+  /**
+   * @returns string. undefined on oversize or EOF.
+   */
+  readString(len?: number, encoding?: string) {
+    const remain = this.remain;
+    let extraLength = 0;
+
+    if (len === undefined) {
+      // C String Mode
+      len = this.#strlen();
+      extraLength = 1;
+    }
+    if (len === undefined || remain < len) return undefined; // EOF
+
+    const buffer = this.peekBuffer(len);
+    try {
+      const string = new TextDecoder(encoding ?? 'utf-8', { fatal: true }).decode(buffer);
+      this.skip(len + extraLength);
+      return string;
+    } catch {
+      return undefined;
+    }
   }
 }
